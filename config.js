329a9a0de581ae82a3f890db3d6302abcca6c018
Soul Lee <lee.soul12@gmail.com>
2022-02-13 16:38:15 -0500

    moved all my backend to root

---
+++ config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+module.exports = {
+  development: {
+    database: 'typing_test1',
+    dialect: 'postgres'
+  },
+  test: {
+    database: 'database_test',
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    version: '1.0.0',
+    engines: {
+      npm: '8.x',
+      node: 'v16.x'
+    },
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+        require: true
+      }
+    }
+  }
+};
